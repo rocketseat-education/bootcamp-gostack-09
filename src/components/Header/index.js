08a1f603bdd9052a3dcec1af0b40d49c8eca5a80
Higo Ribeiro <HORBJN@gmail.com>
2019-11-14 18:48:23 -0300

    fix: Undefined avatar error

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -28,7 +28,7 @@ export default function Header() {
             </div>
             <img
               src={
-                profile.avatar.url ||
+                (profile.avatar && profile.avatar.url) ||
                 'https://api.adorable.io/avatars/50/abott@adorable.png'
               }
               alt="Diego Fernandes"
