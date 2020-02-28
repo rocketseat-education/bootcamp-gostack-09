51b4575b5b4f5e9d40d5632fda44ab67a00e6fdd
Anderson Lottermann Thomé <andersonlottermann@gmail.com>
2020-02-28 11:32:49 -0300

    retirar borda branca que aparece por erro
    para tirar uma borda branca que fica em baixo, retirando o background do _layout e deixando no global.js

--- src/pages/_layouts/auth/styles.js
+++ src/pages/_layouts/auth/styles.js
@@ -3,7 +3,6 @@ import { darken } from 'polished';
 
 export const Wrapper = styled.div`
   height: 100%;
-  background: linear-gradient(-90deg, #7159c1, #ab59c1);
   display: flex;
   justify-content: center;
   align-items: center;
